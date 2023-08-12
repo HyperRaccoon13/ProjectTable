5eb54ab3a5a1e3ca61581e642dbbb0c0f8dec184
Erdragh <enderjanyt@gmail.com>
2023-08-12 18:06:44 +0200

    fix extract_version script to include patch

--- .github/scripts/extract_version.sh
+++ .github/scripts/extract_version.sh
@@ -5,5 +5,6 @@ minecraft_version=${minecraft_grep#minecraft_version=}
 version="${version_grep#mod_version=}"
 
 build="${GITHUB_RUN_NUMBER:-local}"
+[[ -z $PATCH_NUMBER ]] && ([[ $PATCH_NUMBER != 0 ]] && patch=" Patch $PATCH_NUMBER" || patch="" ) || patch=""
 
-echo "$version-$build+mc${minecraft_version}"
+echo "$version-$build+mc${minecraft_version}$patch"
